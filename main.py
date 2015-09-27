e5f0c348a7fbd79857f2520b02d07871a87c57aa
Deep Sukhwani <deepsukhwani@gmail.com>
2015-09-27 20:26:08 +0530

    PSET2 - User Signup Solution implementation

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 import webapp2
 import cgi
 from rot13_implementation import rot13
+import re
 
 form = """
 <form method="post">
@@ -61,6 +62,74 @@ form_rot13 = """
 </form>
 """
 
+form_signup = """
+<head>
+    <title>Sign Up</title>
+    <style type="text/css">
+      .label {text-align: right}
+      .error {color: red}
+    </style>
+
+  </head>
+
+  <body>
+    <h2>Signup</h2>
+    <form method="post">
+      <table>
+        <tr>
+          <td class="label">
+            Username
+          </td>
+          <td>
+            <input type="text" name="username" value="%(username)s">
+          </td>
+          <td class="error">
+          %(usernameError)s
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Password
+          </td>
+          <td>
+            <input type="password" name="password" value="%(password)s">
+          </td>
+          <td class="error">
+            %(passwordError)s
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Verify Password
+          </td>
+          <td>
+            <input type="password" name="verify" value="%(verifyPassword)s">
+          </td>
+          <td class="error">
+            %(verifyPasswordError)s
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Email (optional)
+          </td>
+          <td>
+            <input type="text" name="email" value="%(email)s">
+          </td>
+          <td class="error">
+            %(emailError)s
+          </td>
+        </tr>
+      </table>
+
+      <input type="submit">
+    </form>
+  </body>
+"""
+
 months = ['January',
           'February',
           'March',
@@ -94,6 +163,21 @@ def valid_year(year):
             return year
         else:
             return None
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
+def valid_verifyPassord(password, verifyPassword):
+    return password == verifyPassword
 
 def escape_html(s):
     return cgi.escape(s, quote=True)
@@ -149,6 +233,85 @@ class Rot13Handler(webapp2.RequestHandler):
         # self.response.write(rot_output)
         self.write_form(rot_output)
 
+class SignupHandler(webapp2.RequestHandler):
+
+    USERNAME_ERROR = "That's not a valid username"
+    PASSWORD_ERROR = "That's not a valid password"
+    VERIFY_ERROR = "Your passwords didn't match"
+    EMAIL_ERROR = "That's not a valid email"
+
+    def write_form(self, username="", usernameError="", password="", passwordError="", verifyPassword="", verifyPasswordError="", email="", emailError=""):
+        self.response.write(form_signup % {
+            'username': escape_html(username),
+            'usernameError': usernameError,
+            'password': escape_html(password),
+            'passwordError': passwordError,
+            'verifyPassword': escape_html(verifyPassword),
+            'verifyPasswordError': verifyPasswordError,
+            'email': escape_html(email),
+            'emailError': emailError
+            })
+
+    def get(self):
+        self.write_form()
+
+    def post(self):
+        username_input = self.request.get('username')
+        password_input = self.request.get('password')
+        verifyPassword_input = self.request.get('verify')
+        email_input = self.request.get('email')
+        errorCheck = False
+
+        displayUsernameError = ""
+        displayPasswordError = ""
+        displayVerifyError = ""
+        displayEmailError = ""
+
+        if not valid_username(username_input):
+            displayUsernameError = self.USERNAME_ERROR
+            errorCheck = True
+
+        if not valid_password(password_input):
+            displayPasswordError = self.PASSWORD_ERROR
+            errorCheck = True
+
+        if not valid_verifyPassord(password_input, verifyPassword_input):
+            displayVerifyError = self.VERIFY_ERROR
+            errorCheck = True
+
+        if email_input:
+            if not valid_email(email_input):
+                displayEmailError = self.EMAIL_ERROR
+                errorCheck = True
+
+        if errorCheck == True:
+            self.write_form(username = username_input,
+                usernameError = displayUsernameError,
+                passwordError = displayPasswordError,
+                verifyPasswordError = displayVerifyError,
+                email = email_input,
+                emailError = displayEmailError)
+        else:
+            self.redirect('/pset2/welcome?username=%s' % username_input)
+
+        # if not email_input:
+        #     if valid_username(username_input) and valid_password(password_input) and valid_verifyPassord(password_input, verifyPassword_input):
+        #         self.redirect('/pset2/welcome?username=' + username_input)
+        #     elif not (username_input and password_input and verifyPassword_input):
+        #         self.write_form(usernameError = USERNAME_ERROR, passwordError = PASSWORD_ERROR)
+        #     elif not (valid) 
+
+        # elif not (valid_username and valid_password)
+
+
+
+class WelcomeHandler(webapp2.RequestHandler):
+
+    def get(self):
+        username_input = self.request.get('username')
+        self.response.write('Welcome, %s!' % username_input)
+
+
 # class TestHandler(webapp2.RequestHandler):
 #   def post(self):
 #       # q = self.request.get("q")
@@ -159,5 +322,7 @@ class Rot13Handler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([('/', MainHandler),
                             ('/thanks', ThanksHandler),
-                            ('/pset2/rot13', Rot13Handler)
+                            ('/pset2/rot13', Rot13Handler),
+                            ('/pset2/signup', SignupHandler),
+                            ('/pset2/welcome', WelcomeHandler)
                             ], debug=True)
